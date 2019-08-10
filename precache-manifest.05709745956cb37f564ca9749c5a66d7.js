5cc2ebd911132fccfa69ce06fdf5551593dc8821
Edward Mara <e.james.mara@gmail.com>
2019-08-10 16:18:44 -0700

    Updates

---
+++ precache-manifest.05709745956cb37f564ca9749c5a66d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9afc73cf136c3dc3bf4232ae25132e0d",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "7336db4c9163095a1b40",
+    "url": "/clickyGame/static/css/main.6a4553a4.chunk.css"
+  },
+  {
+    "revision": "c65f0670c7d7a243f52e",
+    "url": "/clickyGame/static/js/2.f75e8bbc.chunk.js"
+  },
+  {
+    "revision": "7336db4c9163095a1b40",
+    "url": "/clickyGame/static/js/main.bb07ad9f.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  },
+  {
+    "revision": "20ae7f174a63342f9c6539581a3beeaa",
+    "url": "/clickyGame/static/media/spaceBG.20ae7f17.jpg"
+  },
+  {
+    "revision": "4586a16608dff9d07782094ed35e82a6",
+    "url": "/clickyGame/static/media/supernovaBG.4586a166.jpg"
+  }
+]);
